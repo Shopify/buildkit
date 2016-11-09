73cd859f13f21be5731ba17ded45a836da60b651
lxfontes <lxfontes@gmail.com>
2016-11-09 13:13:16 -0500

    removing create/delete agent

--- lib/buildkit/client/agents.rb
+++ lib/buildkit/client/agents.rb
@@ -26,29 +26,6 @@ module Buildkit
         get("/v2/organizations/#{org}/agents/#{id}", options)
       end
 
-      # Create an agent
-      #
-      # @param org [String] Organization slug.
-      # @param name [String] The name of the agent.
-      # @return [Sawyer::Resource] Your newly created agent
-      # @see https://buildkite.com/docs/api/agents#create-an-agent
-      # @example Create a new Agent for an organization
-      #   Buildkit.create_agent('my-great-org', 'new-agent')
-      def create_agent(org, name, options = {})
-        post("/v2/organizations/#{org}/agents", options.merge(name: name))
-      end
-
-      # Delete an agent
-      #
-      # @param org [String] Organization slug.
-      # @param id [String] Agent id.
-      # @see https://buildkite.com/docs/api/agents#delete-an-agent
-      # @example Delete an existing agent
-      #   Buildkit.delete_agent('my-great-org', '16940c91-f12d-4122-8154-0edf6c0978c2')
-      def delete_agent(org, id, options = {})
-        delete("/v2/organizations/#{org}/agents/#{id}", options)
-      end
-
       # Stop an agent
       #
       # @param org [String] Organization slug.
