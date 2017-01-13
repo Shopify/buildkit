a22127da0fcae83497561bab213321bea3fcc94f
Steve Lounsbury <steve.lounsbury@shopify.com>
2017-01-13 15:47:18 -0500

    Fix rubocop offenses

--- spec/client/artifacts_spec.rb
+++ spec/client/artifacts_spec.rb
@@ -4,7 +4,7 @@ describe Buildkit::Client::Artifacts do
   context '#artifacts' do
     it 'returns the list of artifacts' do
       VCR.use_cassette 'artifacts' do
-        artifacts = client.artifacts('shopify', 'shopify-branches', 219190, per_page: 2)
+        artifacts = client.artifacts('shopify', 'shopify-branches', 219_190, per_page: 2)
         expect(artifacts.size).to be == 2
 
         artifact = artifacts.first
