0669d0435f0a89c5fb3ac87de6be52551c8a6e2e
Steve Lounsbury <steve.lounsbury@shopify.com>
2017-01-13 15:42:12 -0500

    Add Buildkit client for artifacts (https://buildkite.com/docs/rest-api/artifacts)

---
+++ spec/client/artifacts_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Buildkit::Client::Artifacts do
+  context '#artifacts' do
+    it 'returns the list of artifacts' do
+      VCR.use_cassette 'artifacts' do
+        artifacts = client.artifacts('shopify', 'shopify-branches', 219190, per_page: 2)
+        expect(artifacts.size).to be == 2
+
+        artifact = artifacts.first
+        expect(artifact.state).to be == 'finished'
+        expect(artifact.path).to be == 'lhm.log'
+      end
+    end
+  end
+end
